659048b788f57bcb28e4fc995476a112a0f236c6
Avash <avashmulmi@gmail.com>
2013-07-26 15:22:23 +0545

    initial commit

---
+++ js/begin.js
@@ -0,0 +1,18 @@
+function start(path)
+{
+  window.location=path;
+}
+
+function help()
+{
+  window.location="pages/help.html";
+}
+
+function about()
+{
+  window.location="pages/about.html";
+}
+function goback()
+{
+  window.location="../index.html";
+}
